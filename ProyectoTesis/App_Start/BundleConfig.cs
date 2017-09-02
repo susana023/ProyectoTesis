490ea8915135e77ad23bce3b17229914a600d6ac
Susana023 <a20121886@pucp.pe>
2017-09-02 17:18:43 -0500

    bootstrap theme

--- ProyectoTesis/App_Start/BundleConfig.cs
+++ ProyectoTesis/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace ProyectoTesis
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap_yeti.css",
                       "~/Content/site.css"));
         }
     }
